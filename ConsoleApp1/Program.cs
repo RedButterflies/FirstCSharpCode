5ebf74db8fced71028714f706e30619026266c66
Aleksandra Olejarz <oluta525@gmail.com>
2023-07-31 22:48:20 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+string name = "Jake";
+string lastName = "Kwiatkowski";
+string fullName = "Jakob Kwiatkowski";
+Console.WriteLine(name +" " + lastName);
+Console.WriteLine(fullName);
+
+int a = 5;
+int b = 6;
+Console.WriteLine("wynik: "+a + b);
